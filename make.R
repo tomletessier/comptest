a9da5ff5ab57abf381c0fef6dd5a53c2fef1a7fe
Tom Letessier <tomletessier@gmail.com>
2022-05-09 10:41:43 +0200

    Init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' comptest: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Tom Bech Letessier \email{tomletessier@gmail.com}
+#' 
+#' @date 2022/05/09
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
